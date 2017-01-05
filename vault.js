f3f7bf6f658da7697111facb37eea1af32ea217e
Steve Geluso <stevegeluso@gmail.com>
2017-01-05 11:06:01 -0800

    time based password cracker demo

---
+++ vault.js
@@ -0,0 +1,42 @@
+var now = require("performance-now");
+
+var results = measureDigits();
+for (result in results) {
+  console.log(result, results[result]);
+}
+
+function measureDigits() {
+  var measurements = {};
+
+  for (var n = 0; n < 100000; n++) {
+    for (var i = 0; i < 10; i++) {
+      var digit = i;
+      var guess = "2363" + digit;
+
+      var t0 = now();
+      if (checkPassword(guess + "paaaad") || checkPassword(guess)) {
+        console.log("correct!", guess);
+      };
+      var t1 = now();
+
+      var dt = t1 - t0;
+      if (measurements[digit] === undefined) {
+        measurements[digit] = 0;
+      }
+      measurements[digit] += dt
+    }
+  }
+
+  return measurements;
+}
+
+function checkPassword(guess) {
+  var ACTUAL = "23632"
+  for (var i = 0; i < guess.length || i < ACTUAL.length; i++) {
+    if (ACTUAL[i] !== guess[i]) {
+      return false;
+    }
+  }
+
+  return true;
+}
